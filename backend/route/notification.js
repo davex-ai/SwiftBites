41330f96d754e911192e7389b4f757d6cb4dcc53
dave-ai <omotoyedave4@gmail.com>
2025-12-12 18:08:18 +0100

    notification route

--- backend/route/notification.js
+++ backend/route/notification.js
@@ -1,1 +1,12 @@
+ import express from 'express'
+ import { createNotification, getNotifications, markAsRead, updateOrderStatus } from '../controller/notification';
+ const router = express.Router();
+ 
+ const { protect } = require('../middleware/authMiddleware');
+ 
+ router.post('notification', protect, createNotification);
+ router.get('/my-notification', protect, getNotifications);
+ router.get('/notification/:id', protect,markAsRead);
+ 
+ module.exports = router;
  
